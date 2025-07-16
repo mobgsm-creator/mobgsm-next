89abb5a7c0a27e8c7bca08d75ab2552c395116db
Agney <agneynalapat123@gmail.com>
2025-07-16 15:49:28 +0530

    reloadly test

--- app/reloadly/page.tsx
+++ app/reloadly/page.tsx
@@ -4,7 +4,7 @@ return (
         dangerouslySetInnerHTML={{
           __html: `
             <reloadly-widget data-widget-id="Wb7OEupq5frK7xVWNNCqX7FbUcd6ZNdGAZFSzurdPY"></reloadly-widget>
-            <script src="https://cdn.reloadly.com/widget/v2/reloadly-widget.js" async></script>
+<script src="https://cdn.reloadly.com/widget/v2/reloadly-widget.js"></script>
           `,
         }}
       />
