d989e0fe747650f5d219dc564b1e7fc4598aea39
Agney <agneynalapat123@gmail.com>
2025-08-28 17:42:49 +0530

    cache

--- app/page.tsx
+++ app/page.tsx
@@ -3,40 +3,51 @@ import { headers } from 'next/headers'
 import { getProducts, getBNPL, getESIM, getReloadlyAirtime, getReloadlyGifts, getDevices } from "../lib/supabase"
 export const runtime = 'edge'
 export const dynamic = 'force-dynamic'
-
+let cachedData: any = null;//eslint-disable-line
+let lastFetch = 0;
+const CACHE_DURATION = 72000 * 60 * 1000; // 2 hours
 export default async function HomePage() {
   // Edge runtime request headers aren't available by default in App Router
   // So you fetch them manually with `headers()` from 'next/headers'
   const reqHeaders = headers()
-  
+  const now = Date.now();
   const country =
     reqHeaders.get('x-geo-country') ||
     reqHeaders.get('cf-ipcountry') ||
     reqHeaders.get('x-vercel-ip-country') ||
     'unknown'
-  console.time("fetchAllData");
-  // Fetch all in parallel on the server
-  const [products, bnpl, esim, airtime, giftcards, device_list] =
-    await Promise.all([
+  console.log(now - lastFetch)
+  if (!cachedData || (now - lastFetch) > CACHE_DURATION) {
+    console.time("fetchAllData");
+    // Fetch all in parallel on the server
+    
+    const [products, bnpl, esim, airtime, giftcards, device_list] = await Promise.all([
       getProducts(country),
       getBNPL(),
       getESIM(),
       getReloadlyAirtime(),
       getReloadlyGifts(),
       getDevices(),
-    ])
-  console.timeEnd("fetchAllData");
-  console.log(`products count: ${products.length}, device count: ${device_list.length} esim count: ${esim.length}, airtime count: ${airtime.length}, giftcards count: ${giftcards.length}`);
+    ]);
+  
+    cachedData = { products, bnpl, esim, airtime, giftcards, device_list };
+    lastFetch = now;
+   
+    
+    console.timeEnd("fetchAllData");
+  }
+  //console.log(`products count: ${cachedData!.products.length}, device count: ${device_list.length} esim count: ${esim.length}, airtime count: ${airtime.length}, giftcards count: ${giftcards.length}`);
+
   return (
   
     <HomePageClient
       country_code={country}
-      products={products}
-      bnpl={bnpl}
-      esim={esim}
-      airtime={airtime}
-      giftcards={giftcards}
-      device_list={device_list}
+      products={cachedData!.products}
+      bnpl={cachedData!.bnpl}
+      esim={cachedData!.esim}
+      airtime={cachedData!.airtime}
+      giftcards={cachedData!.giftcards}
+      device_list={cachedData!.device_list}
     />
  
   )
