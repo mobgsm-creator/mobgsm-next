602788559e3149b227e40c42c0d754743ef16fec
Agney <agneynalapat123@gmail.com>
2025-09-20 17:34:02 +0530

    sign out;

--- components/signOut.tsx
+++ components/signOut.tsx
@@ -3,12 +3,11 @@
 
 import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { LogOut } from "lucide-react";
 
 export default function SignOutMenuItem() {
   return (
-    <DropdownMenuItem asChild>
+    
       <Button
         variant="ghost"
         className="w-full justify-start"
@@ -17,6 +16,6 @@ export default function SignOutMenuItem() {
         <LogOut className="mr-2 h-4 w-4" />
         Sign Out
       </Button>
-    </DropdownMenuItem>
+ 
   );
 }
